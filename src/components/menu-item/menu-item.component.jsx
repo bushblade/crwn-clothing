a45b31586329fe83c6a5583cc49c4c088b5a47a3
Will Adams <bushblade@gmail.com>
2019-07-14 21:56:41 +0100

    62 Styling menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,8 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={`${size} menu-item`}>
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
