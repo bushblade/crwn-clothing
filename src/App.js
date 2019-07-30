fac909ed461783e1c7ca27fcbf16e1f9ad04f730
Will Adams <bushblade@gmail.com>
2019-07-30 09:16:09 +0100

    68 shop page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = () => (
-  <div>
-    <Link to='/topics'>Topics</Link>
-    <h1>
-      Hats page{' '}
-      <span role='img' aria-label='hat'>
-        🤠
-      </span>
-    </h1>
-  </div>
-)
-
-const TopicDetail = props => (
-  <div>
-    <h1>Topic Detail page</h1>
-    <h2>{props.match.params.topicId}</h2>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
-        <Route path='/topics/:topicId' component={TopicDetail} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
