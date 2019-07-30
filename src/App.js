22de751e20c44e3b2539e498346b51ca5e31ae7a
Will Adams <bushblade@gmail.com>
2019-07-30 08:34:41 +0100

    66 withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
 
+const HatsPage = () => (
+  <div>
+    <Link to='/topics'>Topics</Link>
+    <h1>
+      Hats page{' '}
+      <span role='img' aria-label='hat'>
+        🤠
+      </span>
+    </h1>
+  </div>
+)
+
+const TopicDetail = props => (
+  <div>
+    <h1>Topic Detail page</h1>
+    <h2>{props.match.params.topicId}</h2>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route path='/topics/:topicId' component={TopicDetail} />
+      </Switch>
     </div>
   )
 }
